42b5102d8221bbe6bdec584e06180510960ba751
nasim67reja <nasimreja67@gmail.com>
2021-09-13 19:43:40 -0700

    small addition to my page content

--- JavaScript/Bankist/starter/script.js
+++ JavaScript/Bankist/starter/script.js
@@ -8,7 +8,8 @@ const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
 
-const openModal = function () {
+const openModal = function (e) {
+  e.preventDefault(); // this will e prevent the default behaviour of a link btn
   modal.classList.remove('hidden');
   overlay.classList.remove('hidden');
 };
@@ -17,9 +18,12 @@ const closeModal = function () {
   modal.classList.add('hidden');
   overlay.classList.add('hidden');
 };
-
-for (let i = 0; i < btnsOpenModal.length; i++)
-  btnsOpenModal[i].addEventListener('click', openModal);
+btnsOpenModal.forEach(el => {
+  el.addEventListener('click', openModal);
+});
+// old school way
+// for (let i = 0; i < btnsOpenModal.length; i++)
+//   btnsOpenModal[i].addEventListener('click', openModal);
 
 btnCloseModal.addEventListener('click', closeModal);
 overlay.addEventListener('click', closeModal);
@@ -29,3 +33,22 @@ document.addEventListener('keydown', function (e) {
     closeModal();
   }
 });
+
+const header = document.querySelector('.header');
+
+const message = document.createElement('div');
+message.classList.add('cookie-message');
+message.textContent = 'Hello world!';
+message.innerHTML =
+  'we love to eat cookie. <button class="btn btn--close-cookie">Got it! </button>';
+console.log(message.textContent);
+// header.append(message);
+header.prepend(message);
+// header.after(message);
+// header.before(message);
+
+document
+  .querySelector('.btn--close-cookie')
+  .addEventListener('click', function () {
+    message.remove();
+  });
