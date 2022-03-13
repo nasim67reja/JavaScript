fcb7896da3f387564fad8351e4f9119c497c6cd3
nasim67reja <nasimreja67@gmail.com>
2022-03-12 16:07:44 -0800

    First Commit 🚀

--- advance JavaScript/Challenges/c1.js
+++ advance JavaScript/Challenges/c1.js
@@ -2,6 +2,100 @@
 // We're building a football betting app (soccer for my American friends �)!
 // Suppose we get data from a web service about a certain game ('game' variable on
 // next page). In this challenge we're gonna work with that data.
+// const game = {
+//   team1: 'Bayern Munich',
+//   team2: 'Borrussia Dortmund',
+//   players: [
+//     [
+//       'Neuer',
+//       'Pavard',
+//       'Martinez',
+//       'Alaba',
+//       'Davies',
+//       'Kimmich',
+//       'Goretzka',
+//       'Coman',
+//       'Muller',
+//       'Gnarby',
+//       'Lewandowski',
+//     ],
+//     [
+//       'Burki',
+//       'Schulz',
+//       'Hummels',
+//       'Akanji',
+//       'Hakimi',
+//       'Weigl',
+//       'Witsel',
+//       'Hazard',
+//       'Brandt',
+//       'Sancho',
+//       'Gotze',
+//     ],
+//   ],
+//   score: '4:0',
+//   scored: ['Lewandowski', 'Gnarby', 'Lewandowski', 'Hummels'],
+//   date: 'Nov 9th, 2037',
+//   odds: {
+//     team1: 1.33,
+//     x: 3.25,
+//     team2: 6.5,
+//   },
+// };
+// Your tasks:
+// 1. Create one player array for each team (variables 'players1' and
+// 'players2')
+// let [...players1] = game.players[0];
+// let [...players2] = game.players[1];
+// 2. The first player in any player array is the goalkeeper and the others are field
+// players. For Bayern Munich (team 1) create one variable ('gk') with the
+// goalkeeper's name, and one array ('fieldPlayers') with all the remaining 10
+// field players
+// let [gk, ...fieldPlayers] = players1;
+// 3. Create an array 'allPlayers' containing all players of both teams (22
+// players)
+// let allPlayers = [...players1, ...players2];
+
+// 4. During the game, Bayern Munich (team 1) used 3 substitute players. So create a
+// new array ('players1Final') containing all the original team1 players plus
+// 'Thiago', 'Coutinho' and 'Perisic'
+// let players1Final = [...players1, 'Thiago', 'Coutinho', 'Perisic'];
+// 5. Based on the game.odds object, create one variable for each odd (called
+// 'team1', 'draw' and 'team2')
+// const { team1, x: draw, team2 } = game.odds;
+// 6. Write a function ('printGoals') that receives an arbitrary number of player
+// names (not an array) and prints each of them to the console, along with the
+// number of goals that were scored in total (number of player names passed in)
+// 7. The team with the lower odd is more likely to win. Print to the console which
+// team is more likely to win, without using an if/else statement or the ternary
+// operator.
+// Test data for 6.: First, use players 'Davies', 'Muller', 'Lewandowski' and 'Kimmich'.
+// Then, call the function again with players from game.scored
+// GOOD LUCK �
+// console.log('Hello World !');
+
+// ////////////////////////////////////////////
+// ////////////////////////////////////////////
+// ////////////////////////////////////////////
+// ////////////////////////////////////////////
+// ////////////////////////////////////////////
+// ////////////////////////////////////////////
+// ////////////////////////////////////////////
+// ////////////////////////////////////////////
+// ////////////////////////////////////////////
+// ////////////////////////////////////////////
+
+// Coding Challenge #1
+// We're building a football betting app (soccer for my American friends �)!
+// Suppose we get data from a web service about a certain game ('game' variable on
+// next page). In this challenge we're gonna work with that data.
+// Your tasks:
+
+// 7. The team with the lower odd is more likely to win. Print to the console which
+// team is more likely to win, without using an if/else statement or the ternary
+
+// GOOD LUCK �
+
 const game = {
   team1: 'Bayern Munich',
   team2: 'Borrussia Dortmund',
@@ -42,34 +136,44 @@ const game = {
     team2: 6.5,
   },
 };
-// Your tasks:
 // 1. Create one player array for each team (variables 'players1' and
 // 'players2')
-let [...players1] = game.players[0];
-let [...players2] = game.players[1];
+const {
+  players: [players1, players2],
+} = game;
+
 // 2. The first player in any player array is the goalkeeper and the others are field
 // players. For Bayern Munich (team 1) create one variable ('gk') with the
 // goalkeeper's name, and one array ('fieldPlayers') with all the remaining 10
 // field players
-let [gk, ...fieldPlayers] = players1;
+const [gk, ...fieldPlayers] = players1;
 // 3. Create an array 'allPlayers' containing all players of both teams (22
 // players)
-let allPlayers = [...players1, ...players2];
-
+const allPlayers = [...players1, ...players2];
 // 4. During the game, Bayern Munich (team 1) used 3 substitute players. So create a
 // new array ('players1Final') containing all the original team1 players plus
 // 'Thiago', 'Coutinho' and 'Perisic'
-let players1Final = [...players1, 'Thiago', 'Coutinho', 'Perisic'];
+const players1Final = [...players1, 'Thiago', 'Coutinho', 'Perisic'];
+
 // 5. Based on the game.odds object, create one variable for each odd (called
 // 'team1', 'draw' and 'team2')
 const { team1, x: draw, team2 } = game.odds;
 // 6. Write a function ('printGoals') that receives an arbitrary number of player
 // names (not an array) and prints each of them to the console, along with the
 // number of goals that were scored in total (number of player names passed in)
-// 7. The team with the lower odd is more likely to win. Print to the console which
-// team is more likely to win, without using an if/else statement or the ternary
 // operator.
 // Test data for 6.: First, use players 'Davies', 'Muller', 'Lewandowski' and 'Kimmich'.
 // Then, call the function again with players from game.scored
-// GOOD LUCK �
-console.log('Hello World !');
+function printGoals(...names) {
+  let firstPlayer = names[0];
+  let numberGoal = 0;
+  for (let i = 0; i < names.length; i++) {
+    if (names[i] === firstPlayer) {
+      firstPlayer = names[i];
+    } else {
+      console.log(`${names[i]} hit's one goal`);
+    }
+  }
+  return names;
+}
+console.log(printGoals('Davies', 'Muller', 'Lewandowski', 'Kimmich'));
